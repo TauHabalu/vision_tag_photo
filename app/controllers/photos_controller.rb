4c7cbf8246863f2b24b5155772f49bc6c20e14fb
Tau Habalu <sergeysoloshenko@gmail.com>
2016-07-11 07:38:03 +0000

    Photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,15 @@
 class PhotosController < ApplicationController
+    
+  permits :file, :detection_type
+
+  def new
+    @photo = Photo.new
+  end
+
+  def upload(photo, detection_type)
+    @photo = Photo.new(photo)
+    @detection_type = detection_type
+    @result = GoogleCloudVision.new(@photo.file.path, detection_type).request
+  end
+  
 end
